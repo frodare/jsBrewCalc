ed9d6738dc1b564259eb7f2c4c73eaa3694ded56
Charles Howard <frodare@gmail.com>
2012-07-02 23:08:53 -0400

    adding more units, need to check the factors

--- brewcalc.js
+++ brewcalc.js
@@ -11,11 +11,11 @@ var BREWCALC = {};
 BREWCALC.units = (function($) {
 	'use strict';
 
-	var u, addUnit, createPowerConverter, unityConverter;
+	var u, addUnit, createBaseUnit, createPowerUnit, createFactorUnit;
 
 	u = {};
 
-	unityConverter = function(val) {
+	createBaseUnit = function() {
 		return {
 			from: function(val) {
 				return val;
@@ -26,8 +26,9 @@ BREWCALC.units = (function($) {
 		};
 	};
 
-	createPowerConverter = function(factor) {
+	createPowerUnit = function(base, factor) {
 		return {
+			base: base,
 			from: function(val) {
 				return val * Math.pow(10, factor);
 			},
@@ -37,15 +38,17 @@ BREWCALC.units = (function($) {
 		};
 	};
 
-	addUnit = function(unit, base, converter) {
-		if (!base) { /* this is a base unit */
-			u[unit] = {};
-		} else {
-			u[base][unit] = converter;
-		}
+	createFactorUnit = function(base, factor) {
+		return {
+			base: base,
+			from: function(val) {
+				return val / factor;
+			},
+			to: function(val) {
+				return val * factor;
+			}
+		};
 	};
-
-
 
 	/* create base units and base units with scalers (g, kg, mg, ...) */
 	var prefixes = ['', 'k', 'c', 'm'];
@@ -56,228 +59,113 @@ BREWCALC.units = (function($) {
 		var base = units[j];
 		for (i = 0; i < prefixes.length; i++) {
 			if (prefixes[i]) {
-				addUnit(prefixes[i] + base, base, createPowerConverter(factors[i]));
+				u[prefixes[i] + base] = createPowerUnit(base, factors[i]);
 			} else { /* base unit */
-				addUnit(base);
+				u[base] = createBaseUnit();
 			}
 		}
 	}
 
+	//TODO: check factors here http://scphillips.com/units/convfact.html
+	/*
+	 * Volume
+	 */
+	u['fl_oz'] = createFactorUnit('l', 33.81402266);
+	u['pt'] = createFactorUnit('l', 2.11337642);
+	u['qt'] = createFactorUnit('l', 1.05668821);
+	u['gal'] = createFactorUnit('l', 0.26417205);
 
 
 	/*
-	 * Color
+	 * Volume
+	 */
+	u['lb'] = createFactorUnit('g', 0.002204622622);
+	u['oz'] = createFactorUnit('g', 0.03527);
+
+	/*
+	 * Volume
 	 */
 
+	/*
+	 * Temperature
+	 */
+	u['C'] = createBaseUnit();
+
+	u['F'] = {
+		base: 'C',
+		from: function (F) {
+			return  (F - 32) / 1.8;
+		},
+		to: function (C) {
+			return (C * 1.8) + 32;
+		}
+	};
+	u['K'] = {
+		base: 'C',
+		from: function (K) {
+			return  K - 273.15;
+		},
+		to: function (C) {
+			return C  + 273.15;
+		}
+	};
 
 
 	/*
-	 * SRM table pull from here: http://www.barleydogbrewery.com/xml/colors.xml
-	 */
-	//TODO: create a formula to replace this table
-	var srm = [];
-	srm[1] = {
-		r: 243,
-		g: 249,
-		b: 147
-	};
-	srm[2] = {
-		r: 248,
-		g: 247,
-		b: 83
-	};
-	srm[3] = {
-		r: 246,
-		g: 245,
-		b: 19
-	};
-	srm[4] = {
-		r: 236,
-		g: 230,
-		b: 26
-	};
-	srm[5] = {
-		r: 224,
-		g: 208,
-		b: 27
-	};
-	srm[6] = {
-		r: 213,
-		g: 188,
-		b: 38
-	};
-	srm[7] = {
-		r: 201,
-		g: 167,
-		b: 50
-	};
-	srm[8] = {
-		r: 191,
-		g: 146,
-		b: 59
-	};
-	srm[9] = {
-		r: 190,
-		g: 140,
-		b: 58
-	};
-	srm[10] = {
-		r: 191,
-		g: 129,
-		b: 58
-	};
-	srm[11] = {
-		r: 190,
-		g: 124,
-		b: 55
-	};
-	srm[12] = {
-		r: 191,
-		g: 113,
-		b: 56
-	};
-	srm[13] = {
-		r: 188,
-		g: 103,
-		b: 51
-	};
-	srm[14] = {
-		r: 178,
-		g: 96,
-		b: 51
-	};
-	srm[15] = {
-		r: 165,
-		g: 89,
-		b: 54
-	};
-	srm[16] = {
-		r: 152,
-		g: 83,
-		b: 54
-	};
-	srm[17] = {
-		r: 141,
-		g: 76,
-		b: 50
-	};
-	srm[18] = {
-		r: 124,
-		g: 69,
-		b: 45
-	};
-	srm[19] = {
-		r: 107,
-		g: 58,
-		b: 30
-	};
-	srm[20] = {
-		r: 93,
-		g: 52,
-		b: 26
-	};
-	srm[21] = {
-		r: 78,
-		g: 42,
-		b: 12
-	};
-	srm[22] = {
-		r: 74,
-		g: 39,
-		b: 39
-	};
-	srm[23] = {
-		r: 54,
-		g: 31,
-		b: 27
-	};
-	srm[24] = {
-		r: 38,
-		g: 23,
-		b: 22
-	};
-	srm[25] = {
-		r: 38,
-		g: 23,
-		b: 22
-	};
-	srm[26] = {
-		r: 25,
-		g: 16,
-		b: 15
-	};
-	srm[27] = {
-		r: 25,
-		g: 16,
-		b: 15
-	};
-	srm[28] = {
-		r: 18,
-		g: 13,
-		b: 12
-	};
-	srm[29] = {
-		r: 16,
-		g: 11,
-		b: 10
-	};
-	srm[30] = {
-		r: 16,
-		g: 11,
-		b: 10
-	};
-	srm[31] = {
-		r: 14,
-		g: 9,
-		b: 8
-	};
-	srm[32] = {
-		r: 15,
-		g: 11,
-		b: 8
-	};
-	srm[33] = {
-		r: 12,
-		g: 9,
-		b: 7
-	};
-	srm[34] = {
-		r: 8,
-		g: 7,
-		b: 7
-	};
-	srm[35] = {
-		r: 8,
-		g: 7,
-		b: 7
-	};
-	srm[36] = {
-		r: 7,
-		g: 6,
-		b: 6
-	};
-	srm[37] = {
-		r: 4,
-		g: 5,
-		b: 4
-	};
-	srm[38] = {
-		r: 4,
-		g: 5,
-		b: 4
-	};
-	srm[39] = {
-		r: 3,
-		g: 4,
-		b: 3
-	};
-	srm[40] = {
-		r: 3,
-		g: 4,
-		b: 3
-	};
+	* Beer Color
+	*/
+	u['SRM'] = createBaseUnit();
 
-	window.SRM = srm;
+	/*
+	 * RGB Color Unit
+	 */
+
+	(function() {
+		function poly(aCoef, x) {
+			var i, out = 0;
+			for (i = 0; i < aCoef.length; i++) {
+				out += aCoef[i] * Math.pow(x, i);
+			}
+			if (out > 255) {
+				out = 255;
+			} else if (out < 0) {
+				return 0;
+			}
+			return parseInt(out, 10);
+		}
+
+		/*
+		 * SRM formula fitted from values in this XML table: http://www.barleydogbrewery.com/xml/colors.xml
+		 */
+
+		function red(srm) {
+			return poly([238.6303585006, 17.6108782693, -8.9883800316, 1.3709404563, -0.1002066713, 0.0037034703, -0.0000669932197182277, 0.000000472459865365479], srm);
+		}
+
+		function green(srm) {
+			return poly([236.4190457383, 24.5851535763, -11.2609108061, 1.3928617335, -0.085670751, 0.0028098906, -0.0000468322823271471, 0.000000311720440784933], srm);
+		}
+
+		function blue(srm) {
+			return poly([246.8168629488, -133.9153654594, 29.8154633754, -3.0816135668, 0.1688592584, -0.0050872708, 0.0000796771402841231, -0.000000507065454065039], srm);
+		}
+
+
+		u['RGB'] = {
+			base: 'SRM',
+			from: function(val) {
+				throw new Error('RGB to SRM not yet supported');
+			},
+			to: function(srm) {
+				return {
+					r: red(srm),
+					g: green(srm),
+					b: blue(srm)
+				};
+			}
+		};
+
+	}());
 
 	/* hex routine from here: http://methodbrewery.com/srm.php */
 	//TODO: there should be a better way to do hex conversion
@@ -287,23 +175,8 @@ BREWCALC.units = (function($) {
 		return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
 	}
 
-	addUnit('SRM');
-	addUnit('RGB', 'SRM', {
-		from: function(val) {
-			throw new Error('RGB to SRM not yet supported');
-		},
-		to: function(val) {
-			val = parseInt(val, 10);
-			if (val < 1) {
-				val = 1;
-			} else if (val > 40) {
-				val = 40;
-			}
-			return srm[val];
-		}
-	});
-
-	addUnit('HTML_RGB', 'SRM', {
+	u['HTML_RGB'] = {
+		base: 'SRM',
 		from: function() {
 			throw new Error('HTML_RGB to SRM not yet supported');
 		},
@@ -311,8 +184,19 @@ BREWCALC.units = (function($) {
 			var rgb = u.SRM.RGB.to(val);
 			return '#' + hex(rgb.r) + hex(rgb.g) + hex(rgb.b);
 		}
-	});
+	};
 
+
+	u.convert = function (fromUnit, toUnit, val) {
+		var fromBase, toBase;
+		fromBase = u[fromUnit].base || fromUnit;
+		toBase = u[toUnit].base || toUnit;
+		if(fromBase !== toBase){
+			throw new Error('Invalid unit convertion, base unit missmatch [' + fromBase + '] != [' + toBase + ']');
+		}
+		console.log('unit');
+		return u[toUnit].to(u[fromUnit].from(val));
+	};
 
 	return u;
 
